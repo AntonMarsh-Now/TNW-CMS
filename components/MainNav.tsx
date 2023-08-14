bb14f759d12c2170757b71e561d6b46fa451524c
keagan <keaganbush12@gmail.com>
2023-08-14 14:09:48 +0200

    edit header

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -13,7 +13,7 @@ export function MainNav() {
     <div className="mr-4 hidden md:flex">
       <Link href="/" className="mr-6 flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">Travelong CMS</span>
+        <span className="hidden font-bold sm:inline-block">TNW-CMS</span>
       </Link>
       <nav className="flex items-center space-x-6 text-sm font-medium">
         <Link
