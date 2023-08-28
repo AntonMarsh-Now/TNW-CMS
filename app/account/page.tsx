5e6b5e150aca0eb4f16f9b4071ca98f9126fa6a8
Bushman22 <keaganbush12@gmail.com>
2023-08-28 14:12:01 +0200

    redirect 2

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,22 @@
 "use client";
-window.location.href = "/account/blogs";
+
+
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 export default function Dashboard() {
   const supabase = createClientComponentClient();
 
+  
   return (
+
+
     <div className="container mt-5 ml-20">
+      <meta http-equiv="refresh" content="0; url='/account/blogs'" />
       <div >Welcome</div>
     </div>
   );
 }
+
+
+
 
